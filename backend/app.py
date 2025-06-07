323ae83be6739d00dc17cc88e09dfe3593bf7273
Vamsi-allam <134683978+Vamsi-allam@users.noreply.github.com>
2025-06-07 17:14:29 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -24,7 +24,7 @@ app = FastAPI(title="Trading View Clone API")
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],  # Add your frontend URLs
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173",''https://trading-view-deployment.vercel.app],  # Add your frontend URLs
     allow_credentials=True,
     allow_methods=["*"],  # Allow all methods including OPTIONS for preflight
     allow_headers=["*"],
